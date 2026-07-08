6abe48ef943ed7aef8b0fd93e4c4ea1ad9783be8
Ed Tao <edtaoisgod@gmail.com>
2026-07-08 19:30:08 +0800

    fix:卡片底色限制為light2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!-- filepath: resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="bg-stone-50/60 text-stone-800 antialiased selection:bg-teal-100 selection:text-teal-900">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light" class="bg-stone-50/60 text-stone-800 antialiased selection:bg-teal-100 selection:text-teal-900">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
